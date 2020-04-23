1ff0f242b587da3f676546be08ceed7eafb9c99d
Milan Kocic <milan,mika98@gmail.com>
2020-04-23 10:50:08 +0200

    klikom tacke, i neka dugmici dodataa

--- TrijangulacijaTacaka/Form1.Designer.cs
+++ TrijangulacijaTacaka/Form1.Designer.cs
@@ -36,6 +36,9 @@
             this.btSaveToFile = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.bt_prost = new System.Windows.Forms.Button();
+            this.bt_hull = new System.Windows.Forms.Button();
+            this.bt_triangulation = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,13 +53,14 @@
             this.pictureBox1.Size = new System.Drawing.Size(728, 346);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
             // 
             // btGenerateNext
             // 
             this.btGenerateNext.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btGenerateNext.Font = new System.Drawing.Font("Microsoft YaHei UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btGenerateNext.ForeColor = System.Drawing.Color.DarkSlateGray;
-            this.btGenerateNext.Location = new System.Drawing.Point(321, 12);
+            this.btGenerateNext.Location = new System.Drawing.Point(28, 12);
             this.btGenerateNext.Name = "btGenerateNext";
             this.btGenerateNext.Size = new System.Drawing.Size(96, 40);
             this.btGenerateNext.TabIndex = 1;
@@ -98,17 +102,57 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // bt_prost
+            // 
+            this.bt_prost.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.bt_prost.Font = new System.Drawing.Font("Microsoft YaHei UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_prost.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.bt_prost.Location = new System.Drawing.Point(205, 12);
+            this.bt_prost.Name = "bt_prost";
+            this.bt_prost.Size = new System.Drawing.Size(108, 40);
+            this.bt_prost.TabIndex = 5;
+            this.bt_prost.Text = "Poligon";
+            this.bt_prost.UseVisualStyleBackColor = true;
+            // 
+            // bt_hull
+            // 
+            this.bt_hull.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.bt_hull.Font = new System.Drawing.Font("Microsoft YaHei UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_hull.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.bt_hull.Location = new System.Drawing.Point(332, 12);
+            this.bt_hull.Name = "bt_hull";
+            this.bt_hull.Size = new System.Drawing.Size(96, 40);
+            this.bt_hull.TabIndex = 6;
+            this.bt_hull.Text = "Hull";
+            this.bt_hull.UseVisualStyleBackColor = true;
+            // 
+            // bt_triangulation
+            // 
+            this.bt_triangulation.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.bt_triangulation.Font = new System.Drawing.Font("Microsoft YaHei UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_triangulation.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.bt_triangulation.Location = new System.Drawing.Point(444, 12);
+            this.bt_triangulation.Name = "bt_triangulation";
+            this.bt_triangulation.Size = new System.Drawing.Size(96, 40);
+            this.bt_triangulation.TabIndex = 7;
+            this.bt_triangulation.Text = "Triang";
+            this.bt_triangulation.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bt_triangulation);
+            this.Controls.Add(this.bt_hull);
+            this.Controls.Add(this.bt_prost);
             this.Controls.Add(this.btSaveToFile);
             this.Controls.Add(this.btFileInput);
             this.Controls.Add(this.btGenerateNext);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -125,6 +169,9 @@
         private System.Windows.Forms.Button btSaveToFile;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button bt_prost;
+        private System.Windows.Forms.Button bt_hull;
+        private System.Windows.Forms.Button bt_triangulation;
     }
 }
 
